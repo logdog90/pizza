5595d8f1afe3bce336cd861043eea2da143d2afe
logdog90 <logdogkrzn@gmail.com>
2020-02-01 13:49:32 +1000

    Загрузил уже готовый сайт в репозиторий

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-icon').onclick = function () {
+    document.querySelector('.menu-icon').classList.toggle('active');
+    document.querySelector('.nav').classList.toggle('nav-active');
+};
